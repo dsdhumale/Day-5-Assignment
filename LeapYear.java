1232c37b442a9a534b1e33e06af5d29d32ae7b12
dsdhumale <112741544+dsdhumale@users.noreply.github.com>
2022-10-26 11:56:01 +0530

    Added : Ability to check year is Leap year or not

---
+++ LeapYear.java
@@ -0,0 +1,18 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter year :");
+		int year = scanner.nextInt();
+		if (year % 4 == 0 && year % 100 != 0) {
+			System.out.println(year + " is leap year ");
+
+		} else if (year % 100 == 0 && year % 400 == 0) {
+			System.out.println(year + " is a leap year");
+		} else
+			System.out.println(year + " is not a leap year");
+	}
+}
